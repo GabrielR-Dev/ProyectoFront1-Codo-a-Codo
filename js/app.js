2aaf40263a79171aaadab6dcac6912d096ed02eb
GabrielR-Dev <gabrielomarrolon1996@gmail.com>
2023-11-03 23:27:31 -0300

    agregando funcionalidad JS

---
+++ js/app.js
@@ -0,0 +1,31 @@
+document.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let oferta = [
+        {
+            "tipo": "estudiante",
+            "oferta": 80
+        },
+        {
+            "tipo": "trainee",
+            "oferta": 50
+        },
+        {
+            "tipo": "junior",
+            "oferta": 15
+        }
+    ];
+
+    let descuento;
+    let cantidad = parseInt(e.target.cantidad.value);
+
+    oferta.forEach(est => {
+        if (est.tipo == e.target.categoria.value) { return descuento = est.oferta }
+    });
+
+    let precioConDescuento = cantidad*(200-(200 * descuento / 100));
+    let pagar = document.getElementById("pagar").textContent = "Total a pagar: $" + precioConDescuento;
+
+
+})
+
